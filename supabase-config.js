15bb5ebd5f26865d75ebc7fcdcc9621506b786b6
carmilainson <milason79@gmail.com>
2026-05-12 22:41:59 +0800

    updated

---
+++ supabase-config.js
@@ -0,0 +1,10 @@
+window.FOODSHARE_SUPABASE = {
+    url: '',
+    anonKey: '',
+    useLocalFallback: true,
+    tables: {
+        users: 'users',
+        posts: 'food_posts',
+        requests: 'food_requests'
+    }
+};
